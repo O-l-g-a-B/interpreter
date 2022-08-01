a53a3c10896dc046f949de30ef81d9d1458fb0ad
O-l-g-a-B <bozhedomova_o@mail.ru>
2022-08-01 09:27:37 +0300

    slider

--- src/Components/ButtonWord.jsx
+++ src/Components/ButtonWord.jsx
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
-
 export const ButtonWord = ({ word }) => {
   const [isPressed, setIsPressed] = useState(false);
   const handleClick = () => {
     setIsPressed(true);
   };
   return isPressed ? (
-    <div>{word.russian}</div>
+    <div>{word.russian || "бассейн"}</div>
   ) : (
-    <Button variant="warning" onClick={handleClick}>
+    <Button variant="warning" className="mb-5" onClick={handleClick}>
       Examination
     </Button>
   );
