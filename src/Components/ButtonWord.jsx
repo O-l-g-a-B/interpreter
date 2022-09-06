6ec1c3c3877c4c0aa900f63c19b034401e516169
O-l-g-a-B <bozhedomova_o@mail.ru>
2022-09-06 12:16:39 +0300

    count

--- src/Components/ButtonWord.jsx
+++ src/Components/ButtonWord.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
-export const ButtonWord = ({ word }) => {
+export const ButtonWord = ({ word, count, setCount }) => {
   const [isPressed, setIsPressed] = useState(false);
   const handleClick = () => {
     setIsPressed(true);
+    setCount(count + 1);
   };
 
   return isPressed ? (
